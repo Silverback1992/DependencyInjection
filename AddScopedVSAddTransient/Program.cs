e0f772a637284775f9ad28550a2ab47cac25ce5c
Gabor Gorog <gabor.gorog26@gmail.com>
2022-11-01 23:20:07 +0100

    Add AddScoped vs AddTransient example demonstrating the difference between them

---
+++ AddScopedVSAddTransient/Program.cs
@@ -0,0 +1,14 @@
+﻿using AddScopedVSAddTransient;
+using Microsoft.Extensions.DependencyInjection;
+
+var collection = new ServiceCollection();
+collection.AddScoped<Scoped>();
+collection.AddTransient<Transient>();
+
+var builder = collection.BuildServiceProvider();
+
+Parallel.For(1, 10, i =>
+{
+    Console.WriteLine($"Scoped ID = {builder.GetService<Scoped>().GetHashCode()}");
+    Console.WriteLine($"Transient ID = {builder.GetService<Transient>().GetHashCode()}");
+});
